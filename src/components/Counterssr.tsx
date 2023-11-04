f47082a0dea298e58a22bb0fcd571121931b03b4
hunghg255 <giahunghust@gmail.com>
2023-11-04 07:42:16 +0700

    imit

---
+++ src/components/Counterssr.tsx
@@ -0,0 +1,14 @@
+'use server';
+
+import React from 'react';
+
+
+const Counterssr = () => {
+  return (
+    <>
+      <h1>Hello Counterssr</h1>
+    </>
+  );
+}
+
+export default Counterssr;
---
+++ src/routes/foo/page.tsx
@@ -0,0 +1,12 @@
+import { Counter } from '../../components/Counter.js';
+import Counterssr from '../../components/Counterssr.js';
+
+const Foo = () => (
+  <div>
+    <h2>Foo</h2>
+    <Counter />
+    <Counterssr />
+  </div>
+);
+
+export default Foo;
